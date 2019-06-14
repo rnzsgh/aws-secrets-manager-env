6b3edbeb80cf2c920b8cd7e3545d10bd82e3a693
Ryan <ryannitz@amazon.com>
2019-06-14 17:48:48 -0400

    Initial

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM golang:1.11.11 as builder
+
+ENV GO111MODULE on
+
+RUN mkdir -p /build
+WORKDIR /build
+RUN useradd -u 10001 app
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o aws-secrets-manager-env main.go
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o test test.go
+
+FROM scratch
+
+COPY --from=builder /build/aws-secrets-manager-env /aws-secrets-manager-env
+COPY --from=builder /build/test /test
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/ssl/certs /etc/ssl/certs
+USER app
+
+ENTRYPOINT ["/aws-secrets-manager-env", "prod/test"]
+
+CMD ["/test"]
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+
+	log "github.com/golang/glog"
+)
+
+func init() {
+	flag.Parse()
+	flag.Lookup("logtostderr").Value.Set("true")
+}
+
+func main() {
+
+	fmt.Println(os.Args)
+
+	if len(os.Args) < 2 {
+		log.Fatalf("You must pass the name of the secrets to load - e.g., aws-secrets-manager-env prod/appA prod/common")
+	}
+
+	region := region()
+	if len(region) == 0 {
+		log.Infof("Outside of AWS - not looking up or setting environment variables from Secrets Manager")
+		return
+	}
+
+	svc := secretsmanager.New(
+		session.New(),
+		aws.NewConfig().WithRegion(region).WithMaxRetries(3),
+	)
+
+	for i := 1; i < len(os.Args); i++ {
+		log.Infof("Loading - secret: %s", os.Args[i])
+		if err := secret(svc, os.Args[i]); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Flush()
+}
+
+func secret(svc *secretsmanager.SecretsManager, name string) error {
+
+	result, err := svc.GetSecretValue(&secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(name),
+		VersionStage: aws.String("AWSCURRENT"),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Unable to load - secret: %s - reason: %v", name, err)
+	}
+
+	m := make(map[string]interface{})
+
+	if err = json.Unmarshal([]byte(*result.SecretString), &m); err != nil {
+		return fmt.Errorf("Unable to parse json - secret: %s - reason: %v:", name, err)
+	}
+
+	for k, v := range m {
+		if err = os.Setenv(k, v.(string)); err != nil {
+			return fmt.Errorf("Unable to set environment variable - secret: %s - reason: %v:", name, err)
+		} else {
+			log.Infof("Setting environment variable - secret: %s - key: %s", name, k)
+		}
+	}
+
+	return nil
+}
+
+// region returns AWS region or an empty string if it is called outside of aws.
+func region() string {
+
+	client := http.Client{Timeout: time.Duration(40 * time.Millisecond)}
+	r, err := client.Get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		panic(fmt.Errorf("Unable to read region lookup body - reason: %v", err))
+	}
+
+	m := make(map[string]interface{})
+
+	if err = json.Unmarshal(body, &m); err != nil {
+		panic(fmt.Errorf("Unable to parse region json - reason: %v:", err))
+	}
+
+	return m["region"].(string)
+}
